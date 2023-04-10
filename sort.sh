b269b9af9eb186f0228259e76aaaeaac0ad746b9
lunat <illeskov@gmail.com>
2023-04-10 22:50:09 +0200

    get servers list

--- sort.sh
+++ sort.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # Get servers list file name
 if [ -z $1 ]; then
-    LISTFILE=$1
-else
     LISTFILE="servers"
+else
+    LISTFILE=$1
 fi
-
+# Check if list exist
+if [ ! -f $LISTFILE ]; then
+    echo "Servers list file $LISTFILE not found"
+    exit 1
+fi
+# Get servers list
+SERVERS=$(cat $LISTFILE)
+# Sorting
+for SERVER in $SERVERS
+do
+    echo "serv: $SERVER"
+done
