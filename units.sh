0fbad5afe51c6ecdb47747f0a94b3472a97f36ae
lunat <illeskov@gmail.com>
2023-04-11 03:48:55 +0200

    finish iterator

--- units.sh
+++ units.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 NAME_STARTS="saby-"
 OLD_DIRECTORY="/opt/misc/"
-NEW_DIRECTORY="/srv/data"
+NEW_DIRECTORY="/srv/data/"
 # Get Units list
-UNITLIST=$(systemctl status $NAME_STARTS* | grep vendor | awk '{print $3}' | sed 's/^.//;s/.$//')
+UNITLIST=$(systemctl show $NAME_STARTS* --type=service | grep FragmentPath | awk -F'=' '{print $2}')
 for UNIT in $UNITLIST
 do
-echo $UNIT
+UNITNAME=$(echo $UNIT | awk -F/ '{print $NF}')
+echo $UNITNAME
+systemctl stop $UNITNAME
+mv  $OLD_DIRECTORY$UNITNAME $NEW_DIRECTORY$UNITNAME
+sed -i "s#$OLD_DIRECTORY#$NEW_DIRECTORY#" $UNIT
+systemctl start $UNITNAME
 done
