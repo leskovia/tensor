27d6185888f3c50bf82de4c62fa52989b18b613e
lunat <illeskov@gmail.com>
2023-04-11 21:18:33 +0200

    iterate with "sort -V"

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 # Check input
-if [ -z $1 && -z $2]; then
- echo "Usage: cleanup.sh dir ver"
- exit 1
+if [[ -z $1 && -z $2 ]]; then
+    echo "Usage: cleanup.sh dir ver"
+    exit 1
 else
     NDIR=$1
     VER=$2
 fi
+if [ ${NDIR: -1} != "/" ]
+then
+    NDIR="${NDIR}/"
+fi
+# Check workdir
+if [ ! -e $NDIR ]; then
+    echo "directory not found"
+    exit 1
+fi
+DISTROS=$(find $NDIR* -maxdepth 1 -type d)
+for DISTR in $DISTROS
+do
+    DISTR_VER=$(echo $DISTR | awk -F_ '{print $NF}')
+    if [ $DISTR_VER = "`echo -e "$DISTR_VER\n$VER" | sort -V -r | head -n1`" ];
+    then
+        echo "new $DISTR_VER"
+    else
+        #Old
+        echo "delete $DISTR"
+        rm -r $DISTR
+    fi
+done
 
